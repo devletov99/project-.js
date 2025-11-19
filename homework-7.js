0a7c13b477bc9924f5289258b6d0c3da13373fc9
Нурбек Девлетов <ndevletov5@gmail.com>
2025-11-19 07:12:37 +0300

    fix

--- homework-7.js
+++ homework-7.js
@@ -2,8 +2,8 @@ import { userComments } from "./comments.js"
 
 // Создать массив чисел от 1 до 10. Отфильтровать его таким образом, что бы мы получил массив чисел, начиная с 5.
 
-const nombers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-const moreFive = nombers.filter(num => num >= 5)
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const moreFive = numbers.filter(num => num >= 5)
 
 // создать массив сторк и проверить есть ли определённая сущность.
 
@@ -13,15 +13,21 @@ console.log(!!knifeKevice)
 
 // Написать функцию, которая аргументом будет принимать массив и изменять его порядок на противоположный.
 
-const changeOrderArrays = [nombers, kitchenDevices].map(object => object.reverse())
+const getReverseArray = array => {
+    return array.reverse(); 
+}
+
+console.log(getReverseArray(numbers))
+
+console.log(getReverseArray(kitchenDevices))
 
 // Вывести в консоль массив тех комментариев, почта пользователей которых содержит ".com"
 
-const mailFilter = userComments.filter(mail => mail.email.toLowerCase() .includes (".com"));
+const mailFilter = userComments.filter(mail => mail.email.toLowerCase().includes(".com"));
 
 // Перебрать массив таким образом, что бы пользователи с id меньше или равно 5 имели postId: 2, а те, у кого id больше 5, имели postId: 1
 
-const postId = userComments.map(user => ({ ...user, postId: user.id <= 5 ? 2:1 }))
+const postId = userComments.map(user => ({ ...user, postId: user.id <= 5 ? 2 : 1 }))
 
  // Перебрать массив, что бы объекты состояли только из айди и имени
 
