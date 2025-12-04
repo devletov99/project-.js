b1eda5af038e79cb5aff8c28aa050b53e9737002
Нурбек Девлетов <ndevletov5@gmail.com>
2025-12-04 13:20:09 +0300

    fix error

--- homework-9.js
+++ homework-9.js
@@ -1,32 +1,34 @@
+// поле для подписки.
+
 const emailForm = document.getElementById('email-form')
 emailForm.addEventListener('submit', (event) => {
   event.preventDefault();
-  const form = event.target
-  const formData = new FormData(form)
+  const form = event.target;
+  const formData = new FormData(form);
   const emailData = Object.fromEntries(formData.entries());
-  console.log(emailData)
-})
+  console.log(emailData);
+});
+
+// форма регистрации.
 
 let user = undefined;
 
-const login = document.getElementById('login').value
-const password = document.getElementById('password').value
-const confirmPassword = document.getElementById('confirm-password').value
-const registForm = document.getElementById('regist-form')
+const login = document.getElementById('login').value;
+const password = document.getElementById('password')
+const confirmPassword = document.getElementById('confirm-password');
+const registForm = document.getElementById('regist-form');
 
 registForm.addEventListener('submit', (event) => {
   event.preventDefault();
-  const form = event.target
+    if (password.value !== confirmPassword.value) {
+      alert('Пароли не совпадают');
+  };
+  const form = event.target;
   const data = new FormData(form);
   user = Object.fromEntries(data.entries());
-    if (password !== confirmPassword) {
-      alert('Пароли не совпадают');
-  } 
-  
   user.createdOn  = new Date();
   console.log(user);
-})
-
+});
 
 // 8. Создать модальное окно, используя классы "modal, modal-showed". Логика такая: при нажатии на кнопку у нас открывается модальное окно путем добавления modal-showed к div с классом modal. Не забываем добавить кнопку для закрытия модалки (крестик в углу).  
 // Важные правила создания модалки:
@@ -35,38 +37,47 @@ registForm.addEventListener('submit', (event) => {
 
 
 const authenticationBtn = document.getElementById('authentication-bttn');
-const containerModal = document.getElementById('container-modal')
-const overlayContainer = document.querySelector('.container-overlay')
+const containerModal = document.getElementById('container-modal');
+const overlayContainer = document.querySelector('.container-overlay');
 
-const elementt = function() {
-  containerModal.classList.add('modal-showed')
-  overlayContainer.classList.add('overlay')
-}
+const openModalBtn = function() {
+  containerModal.classList.add('modal-showed');
+  overlayContainer.classList.add('overlay');
+};
 
-authenticationBtn.addEventListener('click', elementt)
+authenticationBtn.addEventListener('click', openModalBtn);
 
 // для закрытия окна.
 
-const btnClose = document.getElementById('closeBtn')
+const btnClose = document.getElementById('close-btn');
 
 btnClose.addEventListener('click', function() {
   containerModal.classList.remove('modal-showed')
   overlayContainer.classList.remove('overlay')
 })
 
-// 
+//9-10 форма авторизации.
+
+let currentUser = undefined;
+
+const hideModal = () => {
+  containerModal.classList.remove('modal-showed')
+  overlayContainer.classList.remove('overlay')
+}
 
 const formAuthoriz = document.getElementById('form-authorization')
 
 formAuthoriz.addEventListener('submit', (event) => {
   const authorizPassword = document.getElementById('authorization-password').value
   const authorizLogin = document.getElementById('authorization-login').value
-  console.log(authorizPassword, authorizLogin, user.login, user.password)
   event.preventDefault()
     if (authorizLogin === user.login && authorizPassword === user.password){
-      alert('вы авторизовались')
+      alert('вы авторизовались');
+      currentUser = user;
+      currentUser.lastLogin = new Date();
+      hideModal();
   } else{ 
       alert('Неправильно указан логин и/или пароль.')
   }
-  console.log(user)
+  console.log(currentUser)
 })
