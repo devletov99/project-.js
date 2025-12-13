69f7fb3bde08fd42cca5bf4b625766d8a15f6721
Нурбек Девлетов <ndevletov5@gmail.com>
2025-12-13 14:12:15 +0300

    fixed spaces and deleted extra ids

--- form.js
+++ form.js
@@ -3,33 +3,57 @@ class Form {
     this.formId = formId;
     this.form = document.getElementById(formId)
 
-    this.init()
+    this.init();
   }
+
   init() {
     if(!this.form) {
       console.log(`Form: форма с id: '${this.formId}' не найдена`);
       return;
     }
 
-    this.addSubmitHandler(); 
+  this.addSubmitHandler(); 
   }
+
   addSubmitHandler () {
     this.form.addEventListener('submit', (event) => {
       event.preventDefault();
-      this.handleSubmit();
+      if (this.validate()) {
+       this.handleSubmit();
+      }
     });
   }
+
+  validate() {
+    let isValid = true;
+    const formData = new FormData(this.form);
+    const data = Object.fromEntries(formData.entries());
+
+    for (let element of this.form.elements) {
+      if (element.required && !element.value.trim()) {
+        isValid = false;
+        element.classList.add('input-error');
+        return;
+      } else {
+        element.classList.remove('input-error');
+      }
+    }
+    return isValid;
+  }
+
   handleSubmit() {
     const formData = new FormData(this.form);
     const data = Object.fromEntries(formData.entries());
 
     if (data.password !== data.confirmPassword) {
-      alert('Пароли не совпадают');
-      return;
+    alert('Пароли не совпадают');
+    return;
     }
+
     console.log(data) 
 
     this.form.reset();
   }
 }
+
 const form = new Form('registForm')
--- modal.js
+++ modal.js
@@ -1,22 +1,19 @@
 class Modal {
   constructor(modalId, openBtnId, closeBtnId, overlayId) {
-    this.modalId = modalId;
-    this.openBtnId = openBtnId;
-    this.closeBtnId = closeBtnId;
-    this.overlayId = overlayId;
-
+    this.openBtn = document.getElementById(openBtnId);
+    this.closeBtn = document.getElementById(closeBtnId);
     this.modal = document.getElementById(modalId);
     this.overlay = document.getElementById(overlayId)
+
     this.init();
   }
+
   init() {
     if (!this.modal) {
-      console.error(`Modal: элемент с id '${this.modalId}' не найден`);
+      console.error(`Modal: элемент с id ${this.modal} не найден`);
       return;
     }
-    this.openBtn = document.getElementById(this.openBtnId)
-    this.closeBtn = document.getElementById(this.closeBtnId)
-    this.overlay = document.getElementById(this.overlayId)
+
     this.setupEvents();
   }
   setupEvents() {
@@ -26,24 +23,30 @@ class Modal {
         this.open();
       });
     }
-    if(this.closeBtn){
+
+    if(this.closeBtn) {
       this.closeBtn.addEventListener('click', (event) => {
         event.preventDefault();
         this.close();
       });
     }
   }
+
   open() {
-    this.modal.classList.add('modal-showed')
+    this.modal.classList.add('modal-showed');
     this.overlay.classList.add('overlay');
     this.overlay.style.display = 'block';
+
   }
+
     close() {
     this.modal.classList.remove('modal-showed');
     this.overlay.classList.remove('overlay');
   } 
+
   isOpenByClass() {
     return this.modal.classList.contains('modal-showed');
   }
 }
-const modal = new Modal('modalId', 'openBtn', 'closeBtn', 'overlay')
+
+const modal = new Modal('modalId', 'openBtn', 'closeBtn', 'overlay');
