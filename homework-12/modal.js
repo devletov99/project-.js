bd76a47a354c81e94cae99edd0189c34b3fd96d0
Нурбек Девлетов <ndevletov5@gmail.com>
2025-12-23 21:20:41 +0300

    overlay task

---
+++ homework-12/modal.js
@@ -0,0 +1,50 @@
+export class Modal {
+
+  constructor(modalId, buttonId, shouldCloseOnOverlay) {
+    this.modal = document.getElementById(modalId);
+    this.overlay = document.getElementById('overlay');
+    this.#initOpen(buttonId); 
+    this.shouldCloseOnOverlay = shouldCloseOnOverlay;
+  }
+
+  open() {
+    this.modal.classList.add('modal-showed');
+    this.overlay.classList.add('overlay-showed'); 
+
+    this.#initClose(this.shouldCloseOnOverlay);
+  }
+
+  close() { 
+    this.modal.classList.remove('modal-showed');
+    this.overlay.classList.remove('overlay-showed'); 
+
+    this.overlay.removeEventListener('click', this.overlayClickHandler);
+  }
+
+  isOpen() {
+    return this.modal.classList.contains('modal-showed');
+  }
+
+  #initOpen(buttonId) {
+    const button = document.getElementById(buttonId);
+    
+    button.addEventListener('click', () => {
+      this.open();
+    });
+  }
+
+  #initClose() {
+    this.closeButton = this.modal.querySelector('#modal-close-button')
+
+    this.closeButton.addEventListener('click', () => {
+      this.close();
+    });
+
+    if(this.shouldCloseOnOverlay) {
+      this.overlayClickHandler = () => {
+        this.close();
+      };
+      this.overlay.addEventListener('click', this.overlayClickHandler);
+    }   
+  } 
+};
