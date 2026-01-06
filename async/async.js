52c0447203d7cdc034eae121bc08fdf5a0ac9a00
Девлетов Нурбек <ndevletov5@gmail.com>
2026-01-06 22:45:02 +0300

    redid code

--- async/async.js
+++ async/async.js
@@ -1,91 +1,90 @@
 async function getUsers() {
   console.log('Загрузка пользователей...');
 
-    if (!localStorage.getItem('users')) {
-      const response = await fetch('./users.json');
-      const users = await response.json();
+  const hasUsers = localStorage.getItem('users'); 
 
-      await new Promise((resolve, reject) => {
-        setTimeout(() => {
-          try {
-            localStorage.setItem('users', JSON.stringify(users));
-            resolve(users);
+  if(!hasUsers) {
+    const response = await fetch('./users.json');
+    const users = await response.json();
+
+    await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        try {
+          localStorage.setItem('users', JSON.stringify(users));
+          resolve(users);
+      }
+        catch(error) {
+          reject(new Error('Ошибка при загрузке пользователей'));
         }
-          catch (error) {
-            reject(new Error('Ошибка при загрузке пользователей'));
-          }
-        },3000);
-      })  
-      return users;
-    } 
-    else {
-      console.log('пользователи уже загружены');
-    } 
+      },3000);
+    })  
+    return users;
+  } 
+  else {
+    console.log('пользователи уже загружены');
+    return JSON.parse(localStorage.getItem('users'))
+  } 
 };
 
-async function renderUsers() {
-  const users = await getUsers() || JSON.parse(localStorage.getItem('users') || '[]');
+function renderUsers(users = []) {
   
   const userTemplate = document.getElementById('user-template');
   const userList = document.getElementById('users');
-
   const loading = document.getElementById('loading-container');
 
-  try {
-      await new Promise((resolve, reject) => {
-      setTimeout(() => {
-        loading.style.display = 'none';
+  loading.style.display = 'none';
+  userList.innerHTML = '';
 
-        users.forEach(user => {
-          const userClone = userTemplate.content.cloneNode(true);
-          userClone.querySelector('.user-img').src = `../img/${ user.img }.jpg`;
-          userClone.querySelector('.user-id').textContent = `id: ${user.id}`
-          userClone.querySelector('.user-name').textContent = `Имя: ${user.name}`;
-          userClone.querySelector('.user-surname').textContent = `Фамилия: ${user.surname}`;
-          userClone.querySelector('.user-email').textContent = `Электронная почта: ${user.email}`;
-          userClone.querySelector('.user-age').textContent = `Возраст: ${user.age}`;
-          userList.appendChild(userClone);
-        });
-        resolve();
-      }, 3000);
-    })
-  }
+  users.forEach(user => {
+    const userClone = userTemplate.content.cloneNode(true);
 
-  catch {
-    console.error(new Error ('Ошибка при рендеринге пользователей:'));
-  }
+    userClone.querySelector('.user-img').src = `../img/${ user.img }.jpg`;
+    userClone.querySelector('.user-id').textContent = `id: ${ user.id }`
+    userClone.querySelector('.user-name').textContent = `Имя: ${ user.name }`;
+    userClone.querySelector('.user-surname').textContent = `Фамилия: ${ user.surname }`;
+    userClone.querySelector('.user-email').textContent = `Электронная почта: ${ user.email }`;
+    userClone.querySelector('.user-age').textContent = `Возраст: ${ user.age }`;
+
+    userList.appendChild(userClone);
+  });
 }
 
-renderUsers();
+async function init() {
+  const users = await getUsers();
+  renderUsers(users);
+}
+
+init()
 
 // Кнопка для удаления определенной карточки
 
 document.addEventListener('click', (event) => {
-  if (!event.target.classList.contains('btn-delete-user')) return;
-  
-  const userContainer = event.target.closest('.user-container');
-  const userId = Number(userContainer.dataset.id);
-
+  if(!event.target.classList.contains('btn-delete-user')) return;
+    
+  const el = event.target.closest('.user-container')
+  if(!el) return;
+ 
   const user = JSON.parse(localStorage.getItem('users'));
-  const updatedUsers = user.filter(user => user.id !== userId);
+  const updatedUsers = user.filter(user => user.id !== el.dataset.id);
 
   localStorage.setItem('users', JSON.stringify(updatedUsers));
-  userContainer.remove();
+  el.remove();
 });
 
 //Кнопка для удаления всех карточек
 
 const btnDeleteUsers = document.getElementById('btn-delete-them');
-userCards = document.getElementById('users');
 
 btnDeleteUsers.addEventListener('click', () => {
-  userCards.classList.add('users-delete');
+  localStorage.removeItem('users');
+  renderUsers();
 });
 
 // Кнопка для получения всех карточек
 
 const btnShowUsers = document.getElementById('btn-show-all');
 
-btnShowUsers.addEventListener('click', () => {
-  userCards.classList.remove('users-delete');
+btnShowUsers.addEventListener('click', async () => {
+  const users = await getUsers()
+  renderUsers(users)
 });
